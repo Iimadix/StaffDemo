5e05da3bea71af02290cddc18372e3b64dc9e1b5
Iimadix <147740105+Iimadix@users.noreply.github.com>
2025-06-04 18:19:38 +0300

    Delete Zavod/Sample-Test1 directory

--- Zavod/Sample-Test1/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "pch.h"
-#include <gtest/gtest.h>
-#include "../Zavod/Personal.h"
-
-TEST(DriverTest, ConstructorAndGetters) {
-    Driver d(1, "John Doe", 5);
-    EXPECT_EQ(d.getId(), 1);
-    EXPECT_EQ(d.getName(), "John Doe");
-}
