6be7661cc8356211e1c35966f25072566f4d40f9
Iimadix <147740105+Iimadix@users.noreply.github.com>
2025-06-04 18:14:09 +0300

    Delete Zavod directory

--- Zavod/Zavod/Project.cpp
+++
@@ -1,5 +0,0 @@
-#include "Project.h"
-
-Project::Project(const std::string& id, int budget) : id(id), budget(budget) {}
-std::string Project::getId() const { return id; }
-int Project::getBudget() const { return budget; }
